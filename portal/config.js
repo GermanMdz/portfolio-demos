d21324e631df4cbf7ec3a377888b0bdeb11b41fc
German <germanmendoza03@gmail.com>
2026-08-11 14:05:34 -0300

    uso Cloud Run HTTPS URL en vez de rewrites

--- portal/config.js
+++ portal/config.js
@@ -2,5 +2,5 @@
 window.PORTFOLIO_CONFIG = {
   demosOrigin: window.location.hostname === "localhost"
     ? "http://localhost:8080"
-    : "",
+    : "https://portfolio-demos-afgzbch5ra-rj.a.run.app",
 };
